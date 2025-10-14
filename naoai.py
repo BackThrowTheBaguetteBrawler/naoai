7367b07766df7f85be19850ca93fd8245aefd241
Lucca Pirovano <luccapirovano@gmx.com>
2025-10-14 15:53:36 -0400

    added input for testing

--- naoai.py
+++ naoai.py
@@ -1,16 +1,19 @@
 import os
 from openai import OpenAI
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("mario"),
-    base_url="http://10.13.100.50:1234/v1",
-)
+def askAi(prompt)
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=os.environ.get("mario"),
+        base_url="http://10.13.100.50:1234/v1",
+    )
+    
+    response = client.responses.create(
+        model="gpt-4o",
+    #    instructions="You are a smart assistant that talks like a pirate.",
+        input=f"Answer in a pirate accent: {prompt}",
+    )
+    (response.output_text)
+    return(response.output_text)
 
-response = client.responses.create(
-    model="gpt-4o",
-#    instructions="You are a smart assistant that talks like a pirate.",
-    input="Answer in a pirate accent: Who created super mario?",
-)
-
-print(response.output_text)
+print(askAi(input("What will you ask the ai?"))
