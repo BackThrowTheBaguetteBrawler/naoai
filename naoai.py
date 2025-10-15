7194900d50d2379014d69e525579cc933f38d612
Lucca Pirovano <luccapirovano@gmx.com>
2025-10-15 16:09:51 -0400

    fixed even more things

--- naoai.py
+++ naoai.py
@@ -2,6 +2,7 @@ serverIP="10.13.100.50"
 robotIP="10.13.100.49"
 
 import os
+import sys
 import qi
 from openai import OpenAI
 
@@ -16,18 +17,20 @@ def askAi(prompt):
         model="gpt-4o",
     #    instructions="You are a smart assistant that talks like a pirate.",
         input=f"Answer in a pirate accent: {prompt}",
+#        input=f"Answer in a gen-alpha skibidi sigma brainrot rizz accent: {prompt}",
     )
     (response.output_text)
     return(response.output_text)
 
-
-# Connect to the robot
-session = qi.Session()
-session.connect(f"tcp://{robotIP}:9559")
-
-# Create a proxy to the ALTextToSpeech service
-tts = session.service("ALTextToSpeech")
+def connectToNao():
+    # Connect to the robot
+    session = qi.Session()
+    session.connect(f"tcp://{robotIP}:9559")
+    
+    # Create a proxy to the ALTextToSpeech service
+    tts = session.service("ALTextToSpeech")
 
 # Use the text-to-speech service
-tts.say(askAi(input("What will you ask the ai? ")))
-
+#connectToNao()
+#tts.say(askAi(sys.argv[1]))
+print(askAi(sys.argv[1]))
